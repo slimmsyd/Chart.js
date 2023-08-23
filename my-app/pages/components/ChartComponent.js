fcc5ea6d84bc3d2cc3f9fa200b67312e00df9579
Sydney Sanders <84546691+slimmsyd@users.noreply.github.com>
2023-08-23 10:16:36 -0400

    removed chartiq

--- my-app/pages/components/ChartComponent.js
+++ my-app/pages/components/ChartComponent.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useRef } from 'react';
-import { CIQ } from 'chartiq/js/chartiq'; // Adjust the import path based on your setup
+// import React, { useEffect, useRef } from 'react';
 
-const ChartComponent = ({ data }) => {
-  const chartContainerRef = useRef(null);
-  const stxRef = useRef(null);
+// const ChartComponent = ({ data }) => {
+//   const chartContainerRef = useRef(null);
+//   const stxRef = useRef(null);
 
-  useEffect(() => {
-    if (chartContainerRef.current && !stxRef.current) {
-      const stx = new CIQ.ChartEngine({
-        container: chartContainerRef.current,
-        // ...other ChartIQ configuration options
-      });
-      stxRef.current = stx;
-    }
+//   useEffect(() => {
+//     if (chartContainerRef.current && !stxRef.current) {
+//       const stx = new CIQ.ChartEngine({
+//         container: chartContainerRef.current,
+//         // ...other ChartIQ configuration options
+//       });
+//       stxRef.current = stx;
+//     }
 
-    if (stxRef.current && data) {
-      stxRef.current.newChart('symbol', data);
-    }
+//     if (stxRef.current && data) {
+//       stxRef.current.newChart('symbol', data);
+//     }
 
-    return () => {
-      // Clean up code if needed
-      stxRef.current.destroy();
-    };
-  }, [data]);
+//     return () => {
+//       // Clean up code if needed
+//       stxRef.current.destroy();
+//     };
+//   }, [data]);
 
-  return <div ref={chartContainerRef} style={{ width: '100%', height: '400px' }}></div>;
-};
+//   return <div ref={chartContainerRef} style={{ width: '100%', height: '400px' }}></div>;
+// };
 
-export default ChartComponent;
+// export default ChartComponent;
